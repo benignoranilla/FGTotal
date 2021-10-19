89c5331d7f52848d0e818e24a07aaa4cf5e741af
Christian Lévano Pinto <levanopintoc@hotmail.com>
2021-10-19 15:44:16 -0500

    Prueba <---> Mensajes

--- FGTotal/FGTotal/Views/Seguidor/HomePage.xaml.cs
+++ FGTotal/FGTotal/Views/Seguidor/HomePage.xaml.cs
@@ -38,7 +38,7 @@ namespace FGTotal.Views.Seguidor
 
         private async void TapGestureRecognizer_Tapped_4(object sender, EventArgs e)
         {
-            await Navigation.PushModalAsync(new Mensajes());
+            await Navigation.PushModalAsync(new Prueba());
         }
 
         private async void TapGestureRecognizer_Tapped_5(object sender, EventArgs e)
