5ede08b2b43bd2bd0e3ec77076e2a4c42b9b2cfb
Christian Lévano Pinto <levanopintoc@hotmail.com>
2022-01-09 20:50:28 -0500

    Update

--- FGTotal/FGTotal/Views/Jugador/Perfil.xaml.cs
+++ FGTotal/FGTotal/Views/Jugador/Perfil.xaml.cs
@@ -1,14 +1,18 @@
 ﻿
 
+using Amazon;
+using Amazon.CognitoIdentity;
+using Amazon.S3;
+using Amazon.S3.Transfer;
 using Plugin.Media;
 using Plugin.Media.Abstractions;
 using System;
 using System.Collections.Generic;
-
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -56,92 +60,13 @@ namespace FGTotal.Views.Jugador
             Navigation.PushAsync(new Editar());
         }
 
-        //vate async void Camara_Clicked(object sender, EventArgs e)
-        //
-        //
-        // MediaFile file = null;
-        // await CrossMedia.Current.Initialize();
-        //
-        // if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
-        // {
-        //     await DisplayAlert("No camara", "Camara no Habilitada", "Ok");
-        //     return;
-        // }
-        //
-        // file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
-        // {
-        //     SaveToAlbum = true,
-        //     Name = "COP.jpg"
-        // });
-        //
-        // if (file == null)
-        //     return;
-        //
-        //
-        //
-        // //Agregando Imagen
-        //if (Foto1.Source == null)
-        //{
-        //    Foto1.Source = ImageSource.FromStream(() =>
-        //    {
-        //        var stream = file.GetStream();
-        //        file.Dispose();
-        //        return stream;
-        //    });
-        //}
-            //else if (image2.Source == null)
-            //{
-            //    image2.Source = ImageSource.FromStream(() =>
-            //    {
-            //        var stream = file.GetStream();
-            //        file.Dispose();
-            //        return stream;
-            //    });
-            //}
-            //else if (image3.Source == null)
-            //{
-            //    image3.Source = ImageSource.FromStream(() =>
-            //    {
-            //        var stream = file.GetStream();
-            //        file.Dispose();
-            //        return stream;
-            //    });
-            //}
-            //else if (image4.Source == null)
-            //{
-            //    image4.Source = ImageSource.FromStream(() =>
-            //    {
-            //        var stream = file.GetStream();
-            //        file.Dispose();
-            //        return stream;
-            //    });
-            //}
-            //else if (image5.Source == null)
-            //{
-            //    image5.Source = ImageSource.FromStream(() =>
-            //    {
-            //        var stream = file.GetStream();
-            //        file.Dispose();
-            //        return stream;
-            //    });
-            //}
-            //else if (image6.Source == null)
-            //{
-            //    image6.Source = ImageSource.FromStream(() =>
-            //    {
-            //        var stream = file.GetStream();
-            //        file.Dispose();
-            //        return stream;
-            //    });
-            //}
-
-            //Imagen
-
-        //}
-
+        
         private async void Camara_Clicked (object sender, EventArgs e)
         {
-            var foto = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions());
+            var foto = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                { Name = "Temp_Photo.jpg", });
+
+            Preferences.Set("Photo", foto.text);
 
             if(foto != null)
             {
@@ -150,6 +75,50 @@ namespace FGTotal.Views.Jugador
                     return foto.GetStream();
                 });
             }
+        }
+
+        private async void Agregar_Clicked (object sender, EventArgs e)
+        {
+            #region
+            CognitoAWSCredentials credentials = new CognitoAWSCredentials(
+            "us-east-1:263cc401-708c-4bad-8594-9c414f1d2e7d",   //"us-east-1:00000000-0000-0000-0000-000000000000", // Your identity pool ID
+            RegionEndpoint.USEast2 // Region
+            );
+
+            var loggingConfig = AWSConfigs.LoggingConfig;
+            loggingConfig.LogMetrics = true;
+            loggingConfig.LogResponses = ResponseLoggingOption.Always;
+            loggingConfig.LogMetricsFormat = LogMetricsFormatOption.JSON;
+            loggingConfig.LogTo = LoggingOptions.SystemDiagnostics;
+
+            AWSConfigs.AWSRegion = "us-east-2";
+            IAmazonS3 s3Client = new AmazonS3Client(credentials, RegionEndpoint.USEast2);
+
+            AWSConfigsS3.UseSignatureVersion4 = true;
+
+            //var s3Client = new AmazonS3Client(credentials, region);
+            var transferUtility = new TransferUtility(s3Client);
+
+            string path = "/storage/emulated/0/Android/data/com.phoenix.fgtotal/files/Pictures/";
+                //Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+            // Para SUBIR un archivo nuevo
+            transferUtility.Upload(
+              Path.Combine(path, "Temp_Photo_5.jpg"),
+              "bucketfgtotal"
+            );
+
+
+            // Para DESCARGAR un archivo nuevo
+            transferUtility.Download(
+              Path.Combine(path, "imagen_prueba_001"),
+              "bucketfgtotal",
+              "FGTotal_file/Screenshot_1.jpg"
+            );
+            #endregion
+
+            // 
+            Navigation.PushAsync(new Perfil());
         }
     }
 }
